a8ade5f717ee3e01d588702f5361eb1b420b5071
Erik Smistad <esmis676@mtroyal.ca>
2020-10-24 20:27:40 -0600

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,13 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS - could also be used on the routes at the bottom if you want to allow this only on some routes
+// You could also pass in an object of options, like > origin: 'https://subdomain.natours.com'
+// Access-Control-Allow-Origin > *
+app.use(cors());
+
+// Options is a type of HTTP request, like get or patch or delete
+app.options('*', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
